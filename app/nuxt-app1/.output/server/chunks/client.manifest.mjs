ed3e209e171ccbb8af3f4e5368b7d742501b2f79
whylost <whylost@aliyun.com>
2022-05-17 15:10:48 +0800

    🚑

---
+++ app/nuxt-app1/.output/server/chunks/client.manifest.mjs
@@ -0,0 +1,13 @@
+const client_manifest = {
+  "../../node_modules/.pnpm/nuxt@3.0.0-rc.3/node_modules/nuxt/dist/app/entry.mjs": {
+    "file": "entry-d7e13b1d.mjs",
+    "src": "../../node_modules/.pnpm/nuxt@3.0.0-rc.3/node_modules/nuxt/dist/app/entry.mjs",
+    "isEntry": true,
+    "css": [
+      "entry.f211fd22.css"
+    ]
+  }
+};
+
+export { client_manifest as default };
+//# sourceMappingURL=client.manifest.mjs.map
